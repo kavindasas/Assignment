b6ae64dfc8c1593fdc95733df005d00118a3fe71
Sasindu Kavinda <kaviliya123@gmail.com>
2022-06-10 13:57:30 +0530

    Add files via upload

---
+++ MarketRatePredict/MarketRatePredict/MarketingCalculator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketRatePredict
+{
+    public class Date
+    {
+        public byte Index;
+        public double Rate;
+
+        public Date(byte index, double rate)
+        {
+            this.Index = index;
+            this.Rate = rate;
+        }
+    }
+
+    public class Gap
+    {
+        public Date BuyingDate;
+        public Date SellingDate;
+
+        public Gap(Date buyingDate, Date sellingDate)
+        {
+            this.BuyingDate = buyingDate;
+            this.SellingDate = sellingDate;
+        }
+
+        public double GetRateGap()
+        {
+            return this.SellingDate.Rate - this.BuyingDate.Rate;
+        }
+    }
+
+    public class MarketingCalculator
+    {
+        public Gap GetMaxDifference(double[] ratesArray)
+        {
+            Date currentBuyingDate = new Date(0, ratesArray[0]);
+            Date currentSellingDate = new Date(1, ratesArray[1]);//Assumption: Atleast 3 elements are there;
+
+            Gap finalGap = new Gap(currentBuyingDate, currentSellingDate);
+
+            while (currentSellingDate.Index < ratesArray.Length)
+            {
+                for (byte index = (byte)(currentSellingDate.Index + 1); index <= ratesArray.Length - 1; index++)
+                {
+                    if (ratesArray[index] >= currentSellingDate.Rate)
+                        currentSellingDate = new Date(index, ratesArray[index]);
+                }
+
+                for (byte index = (byte)(currentBuyingDate.Index + 1); index < currentSellingDate.Index; index++)
+                {
+                    if (ratesArray[index] <= currentBuyingDate.Rate)
+                        currentBuyingDate = new Date(index, ratesArray[index]);
+                }
+
+                if (currentSellingDate.Rate - currentBuyingDate.Rate >= finalGap.GetRateGap())
+                    finalGap = new Gap(currentBuyingDate, currentSellingDate);
+
+                if (currentSellingDate.Index <= ratesArray.Length - 3)
+                {
+                    currentBuyingDate = new Date((byte)(currentSellingDate.Index + 1), ratesArray[(byte)(currentSellingDate.Index + 1)]);
+                    currentSellingDate = new Date((byte)(currentBuyingDate.Index + 1), ratesArray[(byte)(currentBuyingDate.Index + 1)]);
+                }
+                else
+                    break;
+            }
+
+            return finalGap;
+        }
+
+
+    }
+}
---
+++ MarketRatePredict/MarketRatePredict/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MarketRatePredict;
+
+
+string text = File.ReadAllText(@"C:\Users\sasindu\Downloads\UKCodingChallenge\ChallengeSampleDataSet1.txt");
+
+double[] ratesArray = text.Split(",").Select(Double.Parse).ToArray();
+
+Gap gap = new MarketingCalculator().GetMaxDifference(ratesArray);
+Console.WriteLine(string.Join(",", ratesArray));
+Console.WriteLine(String.Format("{0}({1})", gap.BuyingDate.Index + 1, gap.BuyingDate.Rate));
+Console.WriteLine(String.Format("{0}({1})", gap.SellingDate.Index + 1, gap.SellingDate.Rate));
+
+
+text = File.ReadAllText(@"C:\Users\sasindu\Downloads\UKCodingChallenge\ChallengeSampleDataSet2.txt");
+
+ratesArray = text.Split(",").Select(Double.Parse).ToArray();
+
+Gap gapDataSet2 = new MarketingCalculator().GetMaxDifference(ratesArray);
+Console.WriteLine(string.Join(",", ratesArray));
+Console.WriteLine(String.Format("{0}({1})", gapDataSet2.BuyingDate.Index + 1, gapDataSet2.BuyingDate.Rate));
+Console.WriteLine(String.Format("{0}({1})", gapDataSet2.SellingDate.Index + 1, gapDataSet2.SellingDate.Rate));
+
+
+Console.ReadLine();
